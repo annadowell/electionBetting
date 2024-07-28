f732d2b48ac4f47d804094e272e42d8a89832a46
annadowell <146206333+annadowell@users.noreply.github.com>
2024-07-29 00:13:30 +0100

    n

--- scripts/electionBetting.py
+++ scripts/electionBetting.py
@@ -53,7 +53,8 @@ def CleanName(input):
     secondName = second.capitalize()
     if firstName in df2019Constituency['MemberFirstName'].str.capitalize().values:
         #st.write('yay')
-        if secondName == (df2019Constituency.loc[(df2019Constituency['MemberFirstName'] == firstName, 'MemberSurname')].values(0)):
+        #if (secondName in df2019Constituency['MemberSurname'].str.capitalize().values) & :
+        if secondName == (df2019Constituency.loc[(df2019Constituency['MemberFirstName'] == firstName, 'MemberSurname')]):
             #st.write('yip')
             VoteShare2019 = df2019Constituency.loc[(df2019Constituency['MemberSurname'] == secondName) & (df2019Constituency['MemberFirstName'] == firstName), 'WinningShare'].values[0]
             #Odds = CalculateOdds(VoteShare2019)

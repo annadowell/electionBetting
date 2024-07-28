512a86aabe91bdccd01cefe24d8e5b7920afb188
annadowell <146206333+annadowell@users.noreply.github.com>
2024-07-28 21:18:19 +0100

    new py

--- scripts/electionBetting.py
+++ scripts/electionBetting.py
@@ -1,9 +1,12 @@
 import streamlit as st
 import pandas as pd
+import time
 
 st.image("https://cdn2.iconfinder.com/data/icons/croupier/500/vab895_22_slot_machine_isometric_cartoon_retro_vintage_fruit-512.png")
 
 st.title('Bet on the Election')
+
+st.sidebar.[Tally]
 
 def load_data(url):
     data = pd.read_csv(url)
@@ -41,8 +44,9 @@ def FindtheChange(first, second):
     VoteShare2024 = dfCandidates2024.loc[(dfCandidates2024['CandidateSurname'] == second) & (dfCandidates2024['CandidateFirstName'] == first), 'Share'].values[0]
     Swing = dfCandidates2024.loc[(dfCandidates2024['CandidateSurname'] == second) & (dfCandidates2024['CandidateFirstName'] == first), 'Change'].values[0]
     constituency = dfCandidates2024.loc[(dfCandidates2024['CandidateSurname'] == second) & (dfCandidates2024['CandidateFirstName'] == first), 'Constituency'].values[0]
-    st.write(f'In 2024, they won {VoteShare2024} of the vote in {constituency}, the constituency they contested after the boundaries were redrawn. This was calculated as a swing of {Swing}')
     st.session_state.constituency = constituency
+    st.session_state.SubjectVoteShare = VoteShare2024
+    st.session_state.SubjectSwing = Swing
     return
 
 def FindSuccessor(constituency):
@@ -51,6 +55,10 @@ def FindSuccessor(constituency):
     Result = df2024Constituency.loc[(df2024Constituency['Constituency name'] == constituency), 'Result'].values[0]
     WinningVoteShare = df2024Constituency.loc[(df2024Constituency['Constituency name'] == constituency), 'WinningVoteShare'].values[0]
     st.write(f'The result was a {Result}. The MP who serves the seat they contested is {Mp2024first} {Mp2024second}. They won with a winning vote share of {WinningVoteShare}')
+    st.session_state.newMpSurname = Mp2024second
+    st.session_state.newMpForename = Mp2024first
+    st.session_state.result = Result
+    st.session_state.NewWinningVoteShare = WinningVoteShare
     return Mp2024first, Mp2024second, Result, WinningVoteShare
 
 
@@ -70,18 +78,50 @@ if st.session_state.stage >= 1:
         [None, 'They Won!', 'They lost.'],
         on_change=set_state, args=[2]
     )
+    if Result is 'They Won!':
+        set_state(3)
+    if Result is 'They lost.':
+        set_state(4)
 
-if st.session_state.stage >= 2:
+
+#version for if they guessed winning
+if st.session_state.stage == 3:
+    with st.spinner('Wait for it...'):
+        time.sleep(5)
+    st.success("And the results are...")
     FindtheChange(st.session_state.firstName, st.session_state.secondName)
     FindSuccessor(st.session_state.constituency)
-    Next = st.selectbox(
-        'Gamble again?',
-        [None, 'Yes'],
-        on_change=set_state, args=[0]
-    )
-    if Next is None:
-        set_state(2)
+    if (st.session_state.newMpForename == st.session_state.firstName) & (st.session_state.newMpSurname == st.session_state.secondName):
+        #therefore they were right
+        st.balloon()
+        st.header('You win!')
+        st.write_stream(f'They were indeed re-elected. They won {st.session_state.NewWinningVoteShare} of the vote in {st.session_state.constituency}. This is the constituency they contested after the new boundaries were created. This was calculated as a swing of {st.session_state.SubjectSwing} from 2019.')
+        
+    if (st.session_state.newMpForename != st.session_state.firstName) | (st.session_state.newMpSurname != st.session_state.secondName):
+        #therefore they were wrong, their MP was not r-elected
+        st.audio("audio/sadtrombone.swf.mp3", format="audio/mpeg", autoplay="True")
+        st.header('You Lose.')
+        st.write_stream(f'They were not re-elected. They won only {st.session_state.SubjectVoteShare} of the vote in {st.session_state.constituency}. This is the constituency they contested after the new boundaries were created. This was calculated as a swing of {st.session_state.SubjectSwing} compared with their election in 2019. They were succeeded by {st.session_state.newMpForename} {st.session_state.newMpSurname} who won {st.session_state.NewWinningVoteShare} of the vote. The result was {st.session_state.result}.')
+        st.button('Play Again?', on_click=set_state, args=[0])
 
-
-
+#version for if they guessed losing
+if st.session_state.stage == 3:
+    with st.spinner('Wait for it...'):
+        time.sleep(5)
+    st.success("And the results are...")
+    FindtheChange(st.session_state.firstName, st.session_state.secondName)
+    FindSuccessor(st.session_state.constituency)
+    if (st.session_state.newMpForename == st.session_state.firstName) & (st.session_state.newMpSurname == st.session_state.secondName):
+        #therefore they were wrong they were re-elected
+        st.audio("audio/sadtrombone.swf.mp3", format="audio/mpeg", autoplay="True")
+        st.header('You Lose.')
+        st.write_stream(f'They were actually re-elected! They won {st.session_state.NewWinningVoteShare} of the vote in {st.session_state.constituency}. This was calculated as a swing of {st.session_state.SubjectSwing} from 2019.')
+        st.button('Play Again?', on_click=set_state, args=[0])        
+    if (st.session_state.newMpForename != st.session_state.firstName) | (st.session_state.newMpSurname != st.session_state.secondName):
+        #therefore they were correct, their mp was not re-elected
+        st.balloon()
+        st.header('You win!')
+        st.write_stream(f'They were indeed not re-elected. They won only {st.session_state.SubjectVoteShare} of the vote in {st.session_state.constituency}. This is the constituency they contested after the new boundaries were created. This was calculated as a swing of {st.session_state.SubjectSwing} compared with their election in 2019. They were succeeded by {st.session_state.newMpForename} {st.session_state.newMpSurname} who won {st.session_state.NewWinningVoteShare} of the vote. The result was {st.session_state.result}.')
+        st.button('Play Again?', on_click=set_state, args=[0])
+        
 

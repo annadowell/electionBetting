cab26135524b72612afd89a6116f610d8d4e60ae
annadowell <146206333+annadowell@users.noreply.github.com>
2024-07-28 15:06:49 +0100

    newpy

--- scripts/electionBetting.py
+++ scripts/electionBetting.py
@@ -8,12 +8,17 @@ def load_data(url):
 
 Constituency2019 = ('https://raw.githubusercontent.com/annadowell/electionBetting/main/2019-constituency.csv')
 df2019Constituency = load_data(Constituency2019)
-st.write(df2019Constituency)
+#st.write(df2019Constituency)
 
 Constituency2024 = ('https://raw.githubusercontent.com/annadowell/electionBetting/main/2024-constituency.csv')
 df2024Constituency = load_data(Constituency2024)
-st.write(df2024Constituency)
+#st.write(df2024Constituency)
 
 Candidates2024 = ('https://raw.githubusercontent.com/annadowell/electionBetting/main/2024-candidate.csv')
 dfCandidates2024 = load_data(Candidates2024)
-st.write(dfCandidates2024)
+#st.write(dfCandidates2024)
+
+input = st.text_input("Which MP from the 2019 parliament will you bet on?", "Leo Docherty")
+
+if st.button("Spin"):
+    print('lets do something')
